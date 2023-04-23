c6b14689b21de743ce3ba0ba91993e54e7e24358
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-04-23 13:38:33 +0530

    Added Model & Input Field with Styled Todo List display

--- src/model.ts
+++ src/model.ts
@@ -4,33 +4,33 @@ export interface Todo {
     status: boolean
 }
 
-type Actions = { type: 'add', payload: string }
-    | { type: 'remove', payload: number }
-    | { type: 'done', payload: number }
+// type Actions = { type: 'add', payload: string }
+//     | { type: 'remove', payload: number }
+//     | { type: 'done', payload: number }
 
-const TodoReducer = (state: Todo[], action: Actions) => {
-    switch (action.type) {
-        case 'add':
-            return [
-                ...state,
-                { id: Date.now(), todo: action.payload, status: false }
-            ];
-        case 'done':
-            return state.map((todo) => todo.id === action.payload ? { ...todo, status: !todo.status } : todo);
-        case 'remove':
-            return state.filter((todo) => todo.id !== action.payload);
-        default:
-            return state;
-    }
-};
+// const TodoReducer = (state: Todo[], action: Actions) => {
+//     switch (action.type) {
+//         case 'add':
+//             return [
+//                 ...state,
+//                 { id: Date.now(), todo: action.payload, status: false }
+//             ];
+//         case 'done':
+//             return state.map((todo) => todo.id === action.payload ? { ...todo, status: !todo.status } : todo);
+//         case 'remove':
+//             return state.filter((todo) => todo.id !== action.payload);
+//         default:
+//             return state;
+//     }
+// };
 
-import { useReducer } from 'react';
-import { act } from 'react-dom/test-utils';
+// import { useReducer } from 'react';
+// import { act } from 'react-dom/test-utils';
 
-const ReducerExample = () => {
-    const [state, dispatch] = useReducer(TodoReducer, []);
+// const ReducerExample = () => {
+//     const [state, dispatch] = useReducer(TodoReducer, []);
 
-    return (
-        <div>
-    )
-}
+//     return (
+//         <div>
+//     )
+// }
