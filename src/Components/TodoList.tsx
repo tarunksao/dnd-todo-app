2f4de2c62c2b4552ce7317544e1a7d431b2bc51a
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-04-24 12:01:51 +0530

    Modified Code

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -14,13 +14,13 @@ interface Props {
 const TodoList: React.FC<Props> = ({ todos, setTodos, completedTodo, setCompletedTodo }) => {
     return (
         <div className="container">
-            <Droppable droppableId='todosList'>
+            <Droppable droppableId="TodosList">
                 {
                     (provided) => (
                         <div className="todos" ref={provided.innerRef} {...provided.droppableProps}>
                             <span className="todosHeading">Active Tasks</span>
                             {
-                                todos.map((todo, index) => (
+                                todos?.map((todo, index) => (
                                     <SingleTodo index={index} todo={todo} todos={todos} key={todo.id} setTodos={setTodos} />
                                 ))
                             }
@@ -28,13 +28,13 @@ const TodoList: React.FC<Props> = ({ todos, setTodos, completedTodo, setComplete
                         </div>
                     )}
             </Droppable>
-            <Droppable droppableId='completedTodos'>
+            <Droppable droppableId='TodosRemove'>
                 {
                     (provided) => (
                         <div className="removeTodos" ref={provided.innerRef} {...provided.droppableProps}>
                             <span className="todosHeading">Completed Tasks</span>
                             {
-                                completedTodo.map((todo, index) => (
+                                completedTodo?.map((todo, index) => (
                                     <SingleTodo index={index} todo={todo} todos={completedTodo} key={todo.id} setTodos={setCompletedTodo} />
                                 ))
                             }
