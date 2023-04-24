21629a657ca7b8780967080f30457d8cc209ad56
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-04-24 22:32:27 +0530

    Added Drag and Drop Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import InputField from './Components/InputField';
 import { Todo } from './model';
 import TodoList from './Components/TodoList';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { act } from 'react-dom/test-utils';
 
 const App: React.FC = () => {
 
@@ -20,10 +21,31 @@ const App: React.FC = () => {
   }
 
   const onDragEnd = (result: DropResult) => {
-    console.log(result);
     const { source, destination } = result;
-    console.log(source, destination);
-  }
+
+    if (!destination) return;
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+    let add, active = todos, complete = completedTodo;
+
+    if (source.droppableId === 'TodosList') {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === 'TodosList') {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodo(complete);
+    setTodos(active);
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
